003f848b939e1396391e7625027d9a0f357e6277
computer-drive <ccc123zhy@outlook.com>
2025-02-21 21:11:13 +0800

    refactor(pages.py): 使用MaterialIcon替换SettingCard并优化界面布局

--- libs/pages.py
+++ libs/pages.py
@@ -1,4 +1,4 @@
-from libs.widgets import SettingCard
+from libs.widgets import MaterialIcon as MIcon
 from utility.format import format_capacity, format_time
 from PyQt5.QtWidgets import  (QVBoxLayout, QHBoxLayout, QFrame,
                               QWidget, QSizePolicy, QLabel)
@@ -282,18 +282,6 @@ class LocalPage(QFrame):
         self.setLayout(v_layout)
 
 
-class CollectsPage(QFrame):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-        v_layout = QVBoxLayout()
-        v_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
-
-        # 收藏窗口-标题
-        v_layout.addWidget(LargeTitleLabel("收藏"))
-
-        self.setLayout(v_layout)
-    
 
 class SettingsPage(QFrame):
     def __init__(self, parent=None):
@@ -343,21 +331,21 @@ class SettingsPage(QFrame):
         info_widget = QWidget()
         info_widget.setLayout(info_layout)
 
-        cache_card.addGroup(FIF.INFO, "缓存管理", "查看和管理缓存信息", info_widget)
+        cache_card.addGroup(MIcon("cached") ,"缓存管理", "查看和管理缓存信息", info_widget)
 
         self.cache_perferred_input = SwitchButton()
         self.cache_perferred_input.setOnText("开")
         self.cache_perferred_input.setOffText("关")
-        cache_card.addGroup(FIF.INFO, "缓存优选信息", "获取优选信息时优先使用缓存", self.cache_perferred_input)
+        cache_card.addGroup(MIcon("settings_heart"), "缓存优选信息", "获取优选信息时优先使用缓存", self.cache_perferred_input)
 
         self.cache_collect_input = SwitchButton()
         self.cache_collect_input.setOnText("开")
         self.cache_collect_input.setOffText("关")
-        cache_card.addGroup(FIF.INFO, "缓存收藏信息", "收藏时缓存信息", self.cache_collect_input)
+        cache_card.addGroup(MIcon("star"), "缓存收藏信息", "收藏时缓存信息", self.cache_collect_input)
 
         self.cache_max_size_input = SpinBox()
         self.cache_max_size_input.setRange(0, 1024)
-        cache_card.addGroup(FIF.INFO, "缓存最大大小", "缓存最大大小(单位：MB)", self.cache_max_size_input)
+        cache_card.addGroup(MIcon("storage"), "缓存最大大小", "缓存最大大小(单位：MB)", self.cache_max_size_input)
 
         v_layout.addWidget(cache_card)
 
@@ -367,25 +355,30 @@ class SettingsPage(QFrame):
         update_card.setTitle("更新")
 
         self.check_update_button = PushButton("检查更新")
-        update_card.addGroup(FIF.INFO, "检查更新", "检查更新", self.check_update_button)
+        update_card.addGroup(MIcon("update"), "检查更新", "检查更新", self.check_update_button)
 
         self.change_log_button = PushButton("更新日志")
-        update_card.addGroup(FIF.INFO, "更新日志", "查看更新日志", self.change_log_button)
-
-        self.beta_button = PushButton("Beta测试版选项")
-        update_card.addGroup(FIF.INFO, "Beta测试版选项", "Beta测试版选项", self.beta_button)
+        update_card.addGroup(MIcon("change_circle"), "更新日志", "查看更新日志", self.change_log_button)
 
         v_layout.addWidget(update_card)
 
+        beta_card = GroupHeaderCardWidget()
+        beta_card.setTitle("Beta测试计划")
 
+        self.beta_input = SwitchButton()
+        self.beta_input.setOnText("开")
+        self.beta_input.setOffText("关")
+        beta_card.addGroup(MIcon("deployed_code_update"), "Beta测试计划", "参加Beta测试计划", self.beta_input)
 
+        v_layout.addWidget(beta_card)
 
-        
+        self.info_card = GroupHeaderCardWidget()
+        self.info_card.setTitle("信息")
 
+        self.about_button = PushButton("关于")
+        self.info_card.addGroup(MIcon("info"), "关于", "关于", self.about_button)
 
-
-        
-
+        v_layout.addWidget(self.info_card)
 
 
 class LoginWindow(MessageBoxBase):
@@ -661,9 +654,6 @@ class MainWindow(FluentWindow):
         self.localInterface = logical.LocalPage(self)
         self.localInterface.setObjectName("localInterface")
 
-        self.collectsInterface = logical.CollectsPage(self)
-        self.collectsInterface.setObjectName("collectsInterface")
-
         self.settingInterface = logical.SettingsPage(self)
         self.settingInterface.setObjectName("settingInterface")
 
@@ -678,7 +668,6 @@ class MainWindow(FluentWindow):
         self.addSubInterface(self.preferredInterface, FIF.FLAG, "优选")
 
         self.addSubInterface(self.localInterface, FIF.FOLDER, "本地")
-        self.addSubInterface(self.collectsInterface, FIF.HEART, "收藏")
 
         self.initAvator()
 
