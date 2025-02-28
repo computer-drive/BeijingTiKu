33fafb8336c1813d4c645a662c1ad3f083fb2ded
computer-drive <ccc123zhy@outlook.com>
2025-02-28 22:28:43 +0800

    refactor(main.py): 简化启动逻辑，引入BeijingTiku模块

---
+++ libs/__init__.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from datetime import datetime
+from .log import create_logger
+from .consts import *
+from utility.config import JsonConfig
+from PyQt5.QtWidgets import QApplication
+from .pages import MainWindow
+
+class BeijingTiku:
+    def __init__(self):
+        
+        self.logger = self.createLogger()
+        self.config = self.loadConfig()
+
+    def createLogger(self):
+        now = datetime.now() # 获取当前时间
+        logger = create_logger(LOGGER_NAME, # 创建日志记录器
+                        file_logger_name=f"{now.strftime('%Y-%m-%d')}.log") # 设置日志文件名
+        
+        return logger
+    
+    def loadConfig(self):
+        config = JsonConfig(CONFIG_PATH)
+
+        return config
+    
+    def prepare(self):
+        for path in DEPEND_PATH:
+            if not os.path.exists(path):
+                self.logger.warning(f"Dependency path {path} not found, creating...")
+                os.mkdir(path)
+
+    def run(self):
+
+        self.app = QApplication([])
+
+        self.mainWindow = MainWindow(self.config, self.logger)
+        self.mainWindow.show()
+
+
+        result = self.app.exec_()
+        self.logger.info(f"Application exited with code {result}")
+        return result
--- main.py
+++ main.py
@@ -1,41 +1,6 @@
-import sys
-import os
-from datetime import datetime
-from libs.pages import MainWindow
-from libs.log import create_logger
-from PyQt5.QtWidgets import QApplication
-from utility.config import JsonConfig
-from libs.except_hook import except_hook
-from libs.consts import *
-    
+from libs import BeijingTiku
 
-sys.excepthook = except_hook
+if __name__ == '__main__':
+    app = BeijingTiku()
 
-search_count = 0
-
-now = datetime.now() # 获取当前时间
-logger = create_logger(LOGGER_NAME, # 创建日志记录器
-                       file_logger_name=f"{now.strftime('%Y-%m-%d')}.log") # 设置日志文件名
-
-config = JsonConfig(CONFIG_PATH)
-
-for path in DEPEND_PATH:
-    if not os.path.exists(path):
-        logger.warning(f"Dependency path {path} not found, creating...")
-        os.mkdir(path)
-
-if __name__ == "__main__":
-    logger.info("App Started.")
-
-    app = QApplication(sys.argv)
-
-    w = MainWindow(config, logger)
-        
-    w.show()
-
-    result = app.exec_()
-
-    logger.info(f"App Closed with code {result}")
-        
-    sys.exit(result)
-
+    app.run()
