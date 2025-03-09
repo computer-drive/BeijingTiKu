cc3c9f6e596a3b660ed158ad1293d0846fa02eb9
computer-drive <ccc123zhy@outlook.com>
2025-03-09 19:55:57 +0800

    [1.0.115] 更改(log.py)：修改了日志的输出格式，添加success日志级别

--- libs/log.py
+++ libs/log.py
@@ -4,52 +4,75 @@ import os
 import threading
 from libs.consts import *
 from utility.ansi import fore, back, style
-import inspect
 
-print("Initiating <Moudle> libs.log")
+SUCCESS = 25
 
-print(f"    -<Class> LogFormatter")
+logging.addLevelName(SUCCESS, "SUCCESS")
+
+class AppLogger(logging.Logger):
+    
+
+    def __init__(self, name):
+        super().__init__(name)
+
+    def setClassName(self, class_name, **kwargs):
+        if class_name:
+            kwargs["extra"] = {"class_name": class_name}
+        return kwargs
+        
+    def debug(self, msg, class_name="Main", *args, **kwargs):
+        kwargs = self.setClassName(class_name, **kwargs)
+        super().debug(msg, *args, **kwargs)
+    
+    def info(self, msg, class_name="Main", *args, **kwargs):
+        kwargs = self.setClassName(class_name, **kwargs)
+        super().info(msg, *args, **kwargs)
+
+    def warning(self, msg, class_name="Main", *args, **kwargs):
+        kwargs = self.setClassName(class_name, **kwargs)
+        super().warning(msg, *args, **kwargs)
+
+    def error(self, msg, class_name="Main", *args, **kwargs):
+
+        kwargs = self.setClassName(class_name, **kwargs)
+        super().error(msg, *args, **kwargs)
+
+    def critical(self, msg, class_name="Main", *args, **kwargs):
+        kwargs = self.setClassName(class_name, **kwargs)
+        super().critical(msg, *args, **kwargs)
+    
+    def success(self, msg, class_name="Main", *args, **kwargs):
+        kwargs = self.setClassName(class_name, **kwargs)
+        super().log(SUCCESS, msg, *args, **kwargs)
+
+
 class LogFormatter(colorlog.ColoredFormatter):
     def format(self, record):
 
         record.thread = threading.current_thread().name
-        record.levelname = record.levelname.lower()
         record.color = ""
 
-        frame = inspect.currentframe()
-        if frame:
-            frame = frame.f_back
-            while frame:
-                if frame.f_code.co_filename == record.pathname:
-                    record.class_name = frame.f_locals.get("self").__class__.__name__
-                    if record.class_name == "NoneType":
-                        record.class_name = getattr(record, 'class', 'Main')
-                    break
-                frame = frame.f_back
-        else:
-            record.class_name = "Unknown"
         
 
-        
         match record.levelname:
-            case 'debug':
+            case 'DEBUG':
                 record.color = fore.CYAN
-            case 'info':
+            case 'INFO':
                 record.levelname = f"{fore.BLUE}{record.levelname}{style.RESET}"
-            case 'warning':
+            case 'WARNING':
                 record.color = fore.YELLOW
-            case 'error':
+            case 'ERROR':
                 record.color = fore.RED
-            case 'critical':
+            case 'CRITICAL':
                 record.color = fore.RED
+            case 'SUCCESS':
+                record.color = fore.GREEN
 
         
         return super().format(record)
 
 
-    
-    
-print(f"    -<Function> create_logger")
+
 def create_logger(name:str = "",
                   level:int = logging.INFO,
                   format:str = LOGGER_FORMAT ,
@@ -57,11 +80,12 @@ def create_logger(name:str = "",
                   file_logger_path:str = LOG_PATH ,
                   file_logger_name:str = "app.log"
                   ):
-    logger = colorlog.getLogger(name)
+    logger = AppLogger(name)
     logger.setLevel(level)
 
     formatter = LogFormatter(
         format,
+        datefmt="%H:%M:%S",
         reset=True
     )
 
